92a06066f12f7afe25f0d8e95cb6e8ba5c2180c0
minji <mj00102754@gmail.com>
2022-05-18 16:10:52 +0900

    sPost1

--- goodplassu/src/components/Routers.js
+++ goodplassu/src/components/Routers.js
@@ -2,13 +2,14 @@ import React, { useState } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from '../routes/Home';
 import LogIn from '../routes/LogIn';
+import SPostList from '../routes/sPostList'
 
 const Routers = ({IsLoggedIn}) => {
     return (
         <Router>
             <Routes>
                 { IsLoggedIn ?
-                <Route path='/' element={<Home />}></Route>
+                <Route path='/' element={<SPostList />}></Route>
                 : <Route path = '/' element={<LogIn />}></Route>
                 }
             </Routes>
--- goodplassu/src/routes/sPostList.js
+++ goodplassu/src/routes/sPostList.js
@@ -1,10 +1,17 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
 const SPostList = () => {
-    return (
-        <>
+    const [data, setData] = useState('');
+    useEffect = (() => {
+        axios.get("http://jsonplaceholder.typicode.com/todos/1")
+            .then(res=>setData(res.data))
+    },[]);
 
-        </>
+    return (
+        <div>
+            {data}
+        </div>
     );
 };
 

4a45cc04b276a57ccce72db833a26451ac456bd7
minji <mj00102754@gmail.com>
2022-06-01 21:03:59 +0900

    axios get1

--- goodplassu/src/components/Navigation.js
+++ goodplassu/src/components/Navigation.js
@@ -1,7 +1,6 @@
 import React, { useEffect } from "react";
 import {Link, useNavigate} from "react-router-dom"
 import LogIn from "../routes/Login";
-import * as LogInFunction from "../routes/Login"
 
 const Navigation = () =>{
     useEffect(()=>{},[])

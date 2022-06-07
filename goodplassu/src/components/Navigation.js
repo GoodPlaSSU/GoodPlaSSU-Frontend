e60b101e1e830496a90b3eada72176b0df736d4d
minji <mj00102754@gmail.com>
2022-06-07 21:33:00 +0900

    complete 7

--- goodplassu/src/components/Navigation.js
+++ goodplassu/src/components/Navigation.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect} from "react";
 import {useNavigate} from "react-router-dom";
 import Box from '@mui/material/Box';
 import Tabs from '@mui/material/Tabs';
@@ -30,7 +30,7 @@ const Navigation = () =>{
 
     return(
         <Box sx={{width: '100%', color: "info.contrastText", bgcolor: 'info.main'}}>
-            <Tabs sx={{fontWeight: 'bold'}} textColor="inherit" value={false} centered={true}>
+            {window.innerWidth < '600' ?<Tabs scrollButtons allowScrollButtonsMobile sx={{fontWeight: 'bold'}} variant="scrollable" textColor="inherit" value={false}>     
                 <Typography align='left' sx={{
                     mt: 1,
                     mr: 1,
@@ -43,6 +43,19 @@ const Navigation = () =>{
                 {(localStorage.getItem('ID')!=null) ? <Button sx={{height: 30, mt: 1}} variant="contained" onClick={onLogOutClick}>LOGOUT</Button> :
                 <Button sx={{height: 30, mt: 1}} variant="contained" onClick={onLogInClick}>LOGIN</Button>}
             </Tabs>
+            : <Tabs sx={{fontWeight: 'bold'}} centered={true} textColor="inherit" value={false}>     
+                <Typography align='left' sx={{
+                    mt: 1,
+                    mr: 1,
+                    fontWeight: 'bold'
+                }}
+                variant="h6">GoodplaSSU</Typography>
+                <LinkTab label="선행게시판" href="/" />
+                <LinkTab label="참여게시판" href="/cBoard"/>
+                <LinkTab label="마이페이지" href={(localStorage.getItem('ID')!=null)? "/mypage":"/LogIn"}/>
+                {(localStorage.getItem('ID')!=null) ? <Button sx={{height: 30, mt: 1}} variant="contained" onClick={onLogOutClick}>LOGOUT</Button> :
+                <Button sx={{height: 30, mt: 1}} variant="contained" onClick={onLogInClick}>LOGIN</Button>}
+            </Tabs>}
         </Box>
     );
 };
